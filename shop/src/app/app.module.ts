558e1ce46d2526377ecf2afd742124a919d004ae
Aliaksei Makarankau <amakarankau@infoblox.com>
2018-01-22 13:27:40 +0300

    creating home page instead of app

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { routeList } from './app.routes';
+// import { routeList } from './app.routes';
 
 import { AppComponent } from './app.component';
 
 import { ProductsModule } from './products/index';
 import { CartModule } from './cart/index';
+import { CartListComponent } from './cart/cart-list/cart-list.component';
 
 import { NoContentComponent } from './no-content/no-content.component';
+import { HomeComponent } from './home/home.component';
 
-
+export const routeList: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'cartList', component: CartListComponent},
+  {path: '**', component: NoContentComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoContentComponent
+    NoContentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
