2ab9c347d47cd6854a28612844695e4dd16b4dcc
Aliaksei Makarankau <amakarankau@infoblox.com>
2018-01-28 14:59:37 +0300

    hover for selected cart item

--- shop/src/app/cart/cart-item/cart-item.component.ts
+++ shop/src/app/cart/cart-item/cart-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { CartItem } from './index';
+import { Component, OnInit, Input, Output, EventEmitter, HostListener, HostBinding, ElementRef, Renderer } from '@angular/core';
+import { CartItem } from './cart-item.model';
 
 @Component({
   selector: 'app-cart-item',
@@ -8,19 +8,40 @@ import { CartItem } from './index';
 })
 export class CartItemComponent implements OnInit {
 
+  public selectedClass = 'unSelected';
+
   @Input() public currentItem: CartItem;
 
   @Output() public increase: EventEmitter<{}> = new EventEmitter();
   @Output() public decrease: EventEmitter<{}> = new EventEmitter();
   @Output() public remove: EventEmitter<{}> = new EventEmitter();
 
-  constructor() { }
 
-  upQuantity(): void {
+  @HostListener('mouseover') onMouseOver() {
+    this.selectedClass = 'selected';
+
+    // const itemContainer = this.el.nativeElement.querySelector('.item-container');
+    // this.renderer.setElementStyle(itemContainer, 'background-color', 'whitesmoke');
+  // this.renderer.setElementProperty(itemContainer, '[ngStyle]', '{background-color: blue}');
+  //  this.renderer.setElementAttribute(itemContainer, 'class', 'selected');
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    this.selectedClass = 'unSelected';
+
+    // const itemContainer = this.el.nativeElement.querySelector('.item-container');
+    // this.renderer.setElementStyle(itemContainer, 'background-color', 'white');
+    // this.renderer.setElementProperty(itemContainer, 'ngStyle', 'selected');
+    // this.renderer.setElementAttribute(itemContainer, 'class', 'unSelected');
+  }
+
+  constructor(private el: ElementRef,  private renderer: Renderer) { }
+
+  plusOne(): void {
     this.increase.emit(this.currentItem);
   }
 
-  downQuantity(): void {
+  minusOne(): void {
     this.decrease.emit(this.currentItem);
   }
 
